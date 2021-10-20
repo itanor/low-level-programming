59b10fc91dcdf9e4caf52033934397ded3b14067
Itanor Strapazzon <itanor@gmail.com>
2021-10-19 21:43:27 -0300

    basic assembly hello world

---
+++ hello_proper_exit.asm
@@ -0,0 +1,17 @@
+section .data
+message: db 'hello, world!', 10
+
+section .text
+global _start
+
+_start:
+  mov     rax, 1        ; número da syscall 'write'
+  mov     rdi, 1        ; descritor de stdout
+  mov     rsi, message  ; endereço da string
+  mov     rdx, 14       ; tamanho da string em bytes
+  syscall
+
+  mov     rax, 60       ; número da syscall 'exit'
+  xor     rdi, rdi
+  syscall
+
